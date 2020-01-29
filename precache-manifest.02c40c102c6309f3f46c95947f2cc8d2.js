47b64564bd8e2740682e61800cdb2a2cafe89336
Khalilurrehman <khalil_rehman1993@ymail.com>
2020-01-29 13:49:05 +0400

    Updates

---
+++ precache-manifest.02c40c102c6309f3f46c95947f2cc8d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f2d5039331adbbff7acba0b678ca39",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "884fb81ac66038bf2c21",
+    "url": "/Todo-App/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "e16c86e37f9add1f1a7f",
+    "url": "/Todo-App/static/css/main.ec97a390.chunk.css"
+  },
+  {
+    "revision": "884fb81ac66038bf2c21",
+    "url": "/Todo-App/static/js/2.f0c1211a.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/Todo-App/static/js/2.f0c1211a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e16c86e37f9add1f1a7f",
+    "url": "/Todo-App/static/js/main.eed05cec.chunk.js"
+  },
+  {
+    "revision": "a082400fd7e7e8f53412",
+    "url": "/Todo-App/static/js/runtime-main.1111e716.js"
+  }
+]);
